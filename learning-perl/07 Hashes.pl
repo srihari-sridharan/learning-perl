3068e66b025824cb20f7d8f2a05c65768138fc33
Srihari Sridharan <srihari.sridharan.r@gmail.com>
2020-04-03 15:23:07 +0530

    Update 07 Hashes.pl

--- learning-perl/07 Hashes.pl
+++ learning-perl/07 Hashes.pl
@@ -5,3 +5,57 @@ use warnings;
 use diagnostics;
 use 5.18.0;
 use feature 'say';
+
+my $separator =
+"-------------------------------------------------------------------------------------";
+
+say "Hash are also known as associative arrays!";
+
+my %hash = (
+    one   => 1,
+    two   => 2,
+    three => 3,
+    four  => 4,
+    five  => 5,
+    six   => 6
+);
+
+say "Calling say %hash";
+say %hash;
+say $separator;
+
+say
+"Iterating the hash using a while each produces different order of keys / values. Run again to see a different order of keys.";
+while ( my ( $key, $value ) = each %hash ) {
+    say "$key -> $value";
+}
+say $separator;
+
+say
+"Adding elements to the hash - note the use of \$ as we are operating on an element in the hash";
+$hash{seven} = 7;
+$hash{eight} = 8;
+while ( my ( $key, $value ) = each %hash ) {
+    say "$key -> $value";
+}
+say $separator;
+
+say "Updating the existing elements";
+$hash{seven} = "SEVEN";
+$hash{eight} = "EIGHT";
+while ( my ( $key, $value ) = each %hash ) {
+    say "$key -> $value";
+}
+say $separator;
+
+say "Use 'delete' to delete an element";
+delete $hash{eight};
+while ( my ( $key, $value ) = each %hash ) {
+    say "$key -> $value";
+}
+say $separator;
+
+foreach my $key ( sort ( keys %hash ) ) {
+    my $value = $hash{$key};
+    say "$key -> $value";
+}
