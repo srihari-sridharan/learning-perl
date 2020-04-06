e7414ee985f1530e5f3389214a96dfac0b9f25dd
Srihari Sridharan <srihari.sridharan.r@gmail.com>
2020-04-06 22:28:17 +0530

    Create best-practices.pl

---
+++ learning-perl/best-practices.pl
@@ -0,0 +1,16 @@
+# Consistency
+# ; semicolons
+# {} blocks of code
+# Variable names : my $variable_name;
+# Package: Modules::Person
+# Objects: my $person = Module::Person;
+# Constants: CONSTANT_NAME
+# Functions: sub function_name{}
+
+# Follow practices for writing highly maintainble code
+# Spaces (Vertical and Horizontal), Comments, Naming Conventions, etc.
+# #JavaDoc style description for methods
+# Follow 30s rule
+# Explain constants
+
+# use strict and use warnings (e.g. using @array[index] instead of $array[index] is wrong - this is thrown as a warning)
